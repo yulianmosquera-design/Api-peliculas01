65101b2f361a25646bded701d36bf01b609e6806
yulianmosquera-design <yulian.mosquera@est.iudigital.edu.co>
2026-03-28 16:14:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Datos en memoria
+let generos = [];
+let peliculas = [];
+
+// RUTAS GENEROS
+app.get("/api/generos", (req, res) => {
+  res.json(generos);
+});
+
+app.post("/api/generos", (req, res) => {
+  const genero = req.body;
+  generos.push(genero);
+  res.json(genero);
+});
+
+// RUTAS PELICULAS
+app.get("/api/peliculas", (req, res) => {
+  res.json(peliculas);
+});
+
+app.post("/api/peliculas", (req, res) => {
+  const pelicula = req.body;
+  peliculas.push(pelicula);
+  res.json(pelicula);
+});
+
+// PRUEBA
 app.get("/", (req, res) => {
-  res.send("Backend funcionando ");
+  res.send("Backend funcionando 🚀");
 });
 
 app.listen(3000, () => {
-  console.log("Servidor corriendo");
+  console.log("Servidor corriendo en puerto 3000");
 });
